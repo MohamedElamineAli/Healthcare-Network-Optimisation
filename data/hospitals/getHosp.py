ced95bdede3e2048a551a6d8994e8963da241fd2
Abd-Elhakim-Arabet <146327175+Abd-Elhakim-Arabet@users.noreply.github.com>
2024-05-12 23:23:26 +0100

    Update getHosp.py

--- data/hospitals/getHosp.py
+++ data/hospitals/getHosp.py
@@ -1,16 +1,18 @@
 import json
 
-def getHospitalsfromSpecialities(hospitals, specialities):
+def getHospitalsfromSpecialities(hospitals, specialities, general=false):
     results = []
+    if general:
+        specialities.append("General Emergency")
     if specialities == "all":
         for hospital in hospitals:
-            results.append({"name": hospital["name"], "id": hospital["id"], "c": hospital["x"], "y": hospital["y"]})
+            results.append({"name": hospital["name"], "id": hospital["id"], "x": hospital["x"], "y": hospital["y"]})
         return results
 
     for hospital in hospitals:
         for spc in specialities:
             if spc in hospital["services"]:
-                results.append({"name": hospital["name"], "id": hospital["id"], "c": hospital["x"], "y": hospital["y"]})
+                results.append({"name": hospital["name"], "id": hospital["id"], "x": hospital["x"], "y": hospital["y"]})
                 break
 
     return results
@@ -20,8 +22,9 @@ with open('hospitalWithID.json') as f:
 
 hospitals = data["hospitals"]
 specialities = ["Anatomical Pathology"]
+general = false
 
-hosps = getHospitalsfromSpecialities(hospitals, specialities)
+hosps = getHospitalsfromSpecialities(hospitals, specialities, general)
 i = 0
 for  hosp in hosps:
     i = i +1
