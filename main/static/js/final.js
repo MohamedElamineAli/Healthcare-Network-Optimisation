65bfce0616f59dc5887ef414738082781b07927b
youcef mhammdi bouzina <112803317+Xemum@users.noreply.github.com>
2024-05-17 11:49:18 +0100

    Update final.js

--- main/static/js/final.js
+++ main/static/js/final.js
@@ -1,8 +1,7 @@
-
+let Old_start_marker;
 var map = L.map("map").setView([36.7538, 3.0588], 12);
 var Osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 // Style for the GeoJSON layer
@@ -26,7 +25,7 @@ var geojsonLayer = L.geoJSON(myData, {
 
 // Fit the map view to the boundaries of the GeoJSON data
 map.fitBounds(geojsonLayer.getBounds());
-// ship ------------------------------------------------------------------------
+
 // Create an empty GeoJSON object to hold the hospital data
 var hospitalsGeoJSON = {
   type: "FeatureCollection",
@@ -35,9 +34,7 @@ var hospitalsGeoJSON = {
 
 // Populate the GeoJSON object with hospital data
 for (let hospital of hospitals["hospitals"]) {
-  var servicesString =
-    hospital.services.slice(0, 3).join(", ") +
-    (hospital.services.length > 3 ? "..." : "");
+  var servicesString = hospital.services.slice(0, 3).join(", ") + (hospital.services.length > 3 ? "..." : "");
   var feature = {
     type: "Feature",
     geometry: {
@@ -75,182 +72,176 @@ hospitalLayer.bindPopup(function (layer) {
 // Add the hospital layer to the map
 hospitalLayer.addTo(map);
 
-// ship ------------------------------------------------------------------------
 let startMarker, endMarker;
+let currentPathLayer; // Variable to hold the current path layer
 
 function addFeatureWithDelay(feature, delay) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      L.geoJSON(feature, {
+      const newPathLayer = L.geoJSON(feature, {
         style: geojsonStyle2,
       }).addTo(map);
-      // Zoom the map to fit the bounds of the added feature
-      //map.fitBounds(L.geoJSON(feature).getBounds());
-      resolve(); // Resolve the promise after the delay
+      resolve(newPathLayer); 
     }, delay);
   });
 }
 
-
 async function addFeatures() {
-      //optimal path displaying 
-  if (myLine.paths == null){
-    geojsonStyle2["color"]="#5c00ff"
-  for (let line of myLine.o_path["features"]) {
-    await addFeatureWithDelay(line.geometry,0,true);
+  if (currentPathLayer || Old_start_marker) {
+    map.removeLayer(currentPathLayer); 
+    map.removeLayer(Old_start_marker);
   }
-  addStartMarker();
 
-}else{
-  //paths visualisation
-  if (myLine.vis_type == "local search"){
-    console.log(myLine.vis_type)
-    let i = 0
-    arr_cols = ["#FF5733","#E85644","#D24E55","#BB4666","#A53D77","#8E3488","#782B99","#6122AA","#4B19BB","#3410CC","#5c00ff"]
-    for(let path of myLine.paths){
-      if(i<10) {geojsonStyle2["color"]=arr_cols[i];
-        console.log(i)
-        console.log(geojsonStyle2["color"])
-      }
-        await addFeatureWithDelay(path,2000,false);
-        i=i+1
-    }
-  }
-  else if (myLine.vis_type == "graph search"){
-      console.log(myLine.vis_type)
-      geojsonStyle2["color"]="#FF5733"
-        for(let path of myLine.paths){
-          for (let line of path["features"]) {
-            await addFeatureWithDelay(line.geometry,0,false);
-          }
-        } 
-  }
-    geojsonStyle2["color"]="#5c00ff";
+  let newPathLayerGroup = L.layerGroup(); // for the new path
+
+  if (myLine.paths == null) {
+    geojsonStyle2["color"] = "#5c00ff";
     for (let line of myLine.o_path["features"]) {
-      await addFeatureWithDelay(line.geometry,0);
+      let newPathLayer = await addFeatureWithDelay(line.geometry, 0);
+      newPathLayerGroup.addLayer(newPathLayer);
     }
     addStartMarker();
-}
+  } else {
+    if (myLine.vis_type == "local search") {
+      let i = 0;
+      const arr_cols = ["#FF5733", "#E85644", "#D24E55", "#BB4666", "#A53D77", "#8E3488", "#782B99", "#6122AA", "#4B19BB", "#3410CC", "#5c00ff"];
+      for (let path of myLine.paths) {
+        if (i < 10) {
+          geojsonStyle2["color"] = arr_cols[i];
+        }
+        let newPathLayer = await addFeatureWithDelay(path, 2000);
+        newPathLayerGroup.addLayer(newPathLayer);
+        i++;
+      }
+    } else if (myLine.vis_type == "graph search") {
+      geojsonStyle2["color"] = "#FF5733";
+      for (let path of myLine.paths) {
+        for (let line of path["features"]) {
+          let newPathLayer = await addFeatureWithDelay(line.geometry, 0);
+          newPathLayerGroup.addLayer(newPathLayer);
+        }
+      }
+    }
+
+    geojsonStyle2["color"] = "#5c00ff";
+    for (let line of myLine.o_path["features"]) {
+      let newPathLayer = await addFeatureWithDelay(line.geometry, 0);
+      newPathLayerGroup.addLayer(newPathLayer);
+    }
+    addStartMarker();
+  }
+
+  currentPathLayer = newPathLayerGroup; // Update the reference to the current path layer
+  currentPathLayer.addTo(map); 
 }
 
 function addStartMarker() {
   const startPoint = myLine.start;
   startMarker = L.marker([startPoint[0], startPoint[1]]).addTo(map);
+  Old_start_marker = startMarker;
   startMarker.bindPopup("This is your location").openPopup();
 }
 
 var marker = null; // Variable to hold the marker
-let latitude_1=0;
-let longitude_1=0;
+let latitude_1 = 0;
+let longitude_1 = 0;
+
 map.on("click", async function (e) {
   latitude_1 = e.latlng.lat;
   longitude_1 = e.latlng.lng;
-  // Remove the previous marker if it exists
   if (marker !== null) {
     map.removeLayer(marker);
   }
-  requestData = {
-    "latitude": latitude_1,
-    "longitude": longitude_1
-  }
+  const requestData = {
+    latitude: latitude_1,
+    longitude: longitude_1
+  };
 
   let config = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-  },
-  body: JSON.stringify(requestData),
-  }
+    },
+    body: JSON.stringify(requestData),
+  };
+
   try {
-    const res = await fetch("/bound_checking", config)
-    const data = await res.json()
-    if (data.is_inside_algiers){
+    const res = await fetch("/bound_checking", config);
+    const data = await res.json();
+    if (data.is_inside_algiers) {
       marker = L.marker(e.latlng).addTo(map);
-    }
-    else{
-      alert("please select a location inside algiers");
+    } else {
+      alert("Please select a location inside Algiers");
     }
   } catch (error) {
     console.error("Error fetching data:", error);
   }
-  
 });
-
-
 
 let myLine;
 document.querySelector(".button").addEventListener("click", function () {
   let speciality_1 = document.querySelector(".current").textContent;
   let special_general = document.querySelector("#switch").checked;
-  let search_type= document.querySelector("#search-algo").value;
-  let optimal = getCheckedRadioButton();  
-  
-  if (typeof speciality_1 == "undefined" || speciality_1 == null ||speciality_1=="choose"){
-    alert("enter a speciality");
+  let search_type = document.querySelector("#search-algo").value;
+  let optimal = getCheckedRadioButton();
+
+  if (!speciality_1 || speciality_1 == "choose") {
+    alert("Enter a speciality");
     return;
   }
-  if (latitude_1 ==0 || longitude_1==0 ){
-    alert("enter a starting place");
+  if (latitude_1 == 0 || longitude_1 == 0) {
+    alert("Enter a starting place");
     return;
   }
-   // Define the data you want to send
-const requestData = {
-  latitude: latitude_1,
-  longitude:longitude_1,
-  speciality:speciality_1,
-  special_general:special_general,
-  search_strat:search_type,
-  search_tpe:optimal
-};
-//loading here 
-document.querySelector(".overlay").classList.remove("is-on");
-document.querySelector(".container").classList.add("active");
-document.querySelector(".buttons").classList.add("hide");
-document.querySelector(".overlay").classList.add("hide");
-document.querySelector("#map").classList.add("hide");
-document.querySelector("body").style.display="block";
 
-// Make a POST request to your Flask server
-fetch("/main", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(requestData),
-})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    // Assuming your Flask app responds with JSON data
-    myLine = data;
-    if (typeof myLine !== "undefined" && myLine !== null) {
-      //quit loading here
-      document.querySelector(".container").classList.remove("active");
-      document.querySelector(".buttons").classList.remove("hide");
-      document.querySelector(".overlay").classList.remove("hide");
-      document.querySelector("#map").classList.remove("hide");
-      document.querySelector("body").style.display="flex";
-      addFeatures();
-    } else {
-      console.log("NO data");
-    }
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+  const requestData = {
+    latitude: latitude_1,
+    longitude: longitude_1,
+    speciality: speciality_1,
+    special_general: special_general,
+    search_strat: search_type,
+    search_tpe: optimal
+  };
 
+  document.querySelector(".overlay").classList.remove("is-on");
+  document.querySelector(".container").classList.add("active");
+  document.querySelector(".buttons").classList.add("hide");
+  document.querySelector(".overlay").classList.add("hide");
+  document.querySelector("#map").classList.add("hide");
+  document.querySelector("body").style.display = "block";
+
+  fetch("/main", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      myLine = data;
+      if (myLine) {
+        document.querySelector(".container").classList.remove("active");
+        document.querySelector(".buttons").classList.remove("hide");
+        document.querySelector(".overlay").classList.remove("hide");
+        document.querySelector("#map").classList.remove("hide");
+        document.querySelector("body").style.display = "flex";
+        addFeatures();
+      } else {
+        console.log("No data");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 });
 
 function getCheckedRadioButton() {
-  var radios = document.getElementsByName('visualization');
-  var checkedValue = null;
-  for (var i = 0; i < radios.length; i++) {
-    if (radios[i].checked) {
-      checkedValue = radios[i].value;
-      break;
+  const radios = document.getElementsByName('visualization');
+  for (let radio of radios) {
+    if (radio.checked) {
+      return radio.value;
     }
   }
-  return checkedValue;
+  return null;
 }
-
