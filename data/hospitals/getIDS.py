84b59c90d048c13b347eb1861dcab74774de323e
MHND09 <131745373+MHND09@users.noreply.github.com>
2024-05-17 02:39:40 +0100

    Revert "Add files via upload"
    This reverts commit 5eca48f86d7fab6d0359d3d72046a48ba8bff7ff.

--- data/hospitals/getIDS.py
+++ data/hospitals/getIDS.py
@@ -1,12 +1,11 @@
 import json
 import osmnx as ox
-from osmnx import distance
 import geopandas as gpd
 import networkx as nx
 
 place_name = "Algiers, Algeria"
-network_type = 'drive'
-graph = ox.graph_from_place(place_name, network_type=network_type, simplify=True)
+
+graph = ox.graph_from_place(place_name, network_type="all")
 
 
 with open('hopitaux.json') as f:
@@ -14,7 +13,7 @@ with open('hopitaux.json') as f:
 for hospital in data["hospitals"]:
     theX = hospital["x"]
     theY = hospital["y"]
-    id = distance.nearest_nodes(graph, X=theX, Y=theY)
+    id = ox.distance.nearest_nodes(graph, X=theX, Y=theY)
     print(id)
     # print(f"{hospital["name"]}: {id}")
     hospital["id"] = id
